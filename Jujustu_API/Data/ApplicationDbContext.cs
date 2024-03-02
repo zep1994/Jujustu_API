6a516a3910e533848fd2bca199e5d081c518e1a8
Wes Matlock <thomasmatlockbba@gmail.com>
2024-03-01 19:17:43 -0600

    Add project files.

---
+++ Jujustu_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Jujustu_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<YourModel> YourModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<YourModel>().ToTable("YourTableName");
+        }
+    }
